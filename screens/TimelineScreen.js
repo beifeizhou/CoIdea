03a2eb7d31cd58f45f9e0d67a5dcf3dfd732a9c4
Beifei Zhou <z.beifei@gmail.com>
2021-05-02 12:04:10 +0100

    Use json server to load data

--- screens/TimelineScreen.js
+++ screens/TimelineScreen.js
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Timeline from 'react-native-timeline-flatlist'
 import { View, StyleSheet } from 'react-native'
 
-const placeholders = [
-    { time: '09:00', title: 'Event 1', description: 'Event 1 Description' },
-    { time: '10:45', title: 'Event 2', description: 'Event 2 Description' },
-    { time: '12:00', title: 'Event 3', description: 'Event 3 Description' },
-    { time: '14:00', title: 'Event 4', description: 'Event 4 Description' },
-    { time: '16:30', title: 'Event 5', description: 'Event 5 Description' }
-]
+// const placeholders = [
+//     { time: '09:00', title: 'Event 1', description: 'Event 1 Description' },
+//     { time: '10:45', title: 'Event 2', description: 'Event 2 Description' },
+//     { time: '12:00', title: 'Event 3', description: 'Event 3 Description' },
+//     { time: '14:00', title: 'Event 4', description: 'Event 4 Description' },
+//     { time: '16:30', title: 'Event 5', description: 'Event 5 Description' }
+// ]
 
 const TimelineScreen = () => {
+    const [events, setEvents] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/events')
+            .then(res => { return res.json() })
+            .then(data => { setEvents(data) })
+    }, [])
+
     return (
-        <Timeline data={placeholders}
+        <Timeline data={events}
             circleSize={20}
             circleColor='rgb(45,156,219)'
             lineColor='rgb(45,156,219)'
