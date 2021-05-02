c9437803d8f80ea3033dd198a1caa2be7fe3a424
Beifei Zhou <z.beifei@gmail.com>
2021-05-02 13:26:54 +0100

    Add a button

--- screens/TimelineScreen.js
+++ screens/TimelineScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Timeline from 'react-native-timeline-flatlist'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Text, TextInput } from 'react-native'
+import { Button, Icon } from 'react-native-elements'
 
 // const placeholders = [
 //     { time: '09:00', title: 'Event 1', description: 'Event 1 Description' },
@@ -19,23 +20,50 @@ const TimelineScreen = () => {
             .then(data => { setEvents(data) })
     }, [])
 
+    const addEvent = () => {
+        alert('You tapped the button!')
+    }
+
+    const addTask = async (task) => {
+        const res = await fetch('http://localhost:5000/tasks', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(task)
+        })
+        const data = await res.json()
+        setTasks([...tasks, data])
+    }
+
     return (
-        <Timeline data={events}
-            circleSize={20}
-            circleColor='rgb(45,156,219)'
-            lineColor='rgb(45,156,219)'
-            timeContainerStyle={{ minWidth: 52, marginTop: -5 }}
-            timeStyle={{ textAlign: 'center', backgroundColor: '#ff9797', color: 'white', padding: 5, borderRadius: 13 }}
-            descriptionStyle={{ color: 'gray' }}
-            options={{
-                style: {
-                    paddingTop: 20,
-                    paddingLeft: 20,
-                }
-            }}
-        >
-        </Timeline>
+
+        <View style={styles.container}>
+
+            <Icon name="add" color="blue" size='30' />
+            <Timeline data={events}
+                circleSize={20}
+                circleColor='rgb(45,156,219)'
+                lineColor='rgb(45,156,219)'
+                timeContainerStyle={{ minWidth: 52, marginTop: -5 }}
+                timeStyle={{ textAlign: 'center', backgroundColor: '#ff9797', color: 'white', padding: 5, borderRadius: 13 }}
+                descriptionStyle={{ color: 'gray' }}
+                options={{
+                    style: {
+                        paddingTop: 20,
+                        paddingLeft: 20,
+                    }
+                }}
+            >
+            </Timeline>
+        </View>
     )
 }
 
 export default TimelineScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    }
+});
